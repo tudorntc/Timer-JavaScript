33b410577b94a2b74137ec45cac79bf11f48109e
Nica Tudor <zatalo.nox@gmail.com>
2023-05-19 21:50:33 +0300

    stopTimer() function

--- timer.js
+++ timer.js
@@ -4,3 +4,7 @@ let timerInterval;
 function startTimer() {
   timerInterval = setInterval(incrementTimer, 1000); // Increment the timer every second (1000 milliseconds)
 }
+
+function stopTimer() {
+  clearInterval(timerInterval);
+}
